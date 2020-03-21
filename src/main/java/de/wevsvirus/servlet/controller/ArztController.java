72ea6d3bcb32d55174b5827a69802d212f022e64
Jan Giesenberg <jan@giesenberg.de>
2020-03-21 23:27:18 +0100

    Return correct object type

--- src/main/java/de/wevsvirus/servlet/controller/ArztController.java
+++ src/main/java/de/wevsvirus/servlet/controller/ArztController.java
@@ -1,14 +1,13 @@
 package de.wevsvirus.servlet.controller;
 
 import de.wevsvirus.facades.ArztFacade;
-import de.wevsvirus.model.ArztModel;
+import de.wevsvirus.servlet.controller.data.JsonResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/arzt")
@@ -18,7 +17,7 @@ public class ArztController {
     private ArztFacade arztFacade;
 
     @GetMapping("/{plz}")
-    public List<ArztModel> findArztByPLZ(final @PathVariable String plz) {
-        return arztFacade.findArztByPLZ(plz);
+    public JsonResponse findArztByPLZ(final @PathVariable String plz) {
+        return JsonResponse.withData(arztFacade.findArztByPLZ(plz));
     }
 }
